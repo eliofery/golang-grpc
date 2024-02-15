bf2d9edfcfc2ab0a3bb8644ae9af12ee2104ca20
Sergey Erofeev <erofsey@yandex.kz>
2024-02-15 07:11:12 +0600

    Refactoring (fx)

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"log"
+
 	"github.com/eliofery/golang-fullstack/docs/cli"
-	"github.com/eliofery/golang-fullstack/internal/app"
-	"github.com/eliofery/golang-fullstack/internal/config"
+	"github.com/eliofery/golang-fullstack/internal/libs/config"
+	"github.com/eliofery/golang-fullstack/internal/libs/logger"
+	"github.com/eliofery/golang-fullstack/internal/libs/server"
+	"github.com/eliofery/golang-fullstack/pkg/database/postgres"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
 )
 
 func main() {
 	fx.New(
-		fx.WithLogger(func(cfg *config.Config) fxevent.Logger {
-			return cfg.NewLogger()
+		fx.WithLogger(func(config *config.Config) fxevent.Logger {
+			return logger.New(config)
 		}),
 
 		fx.Provide(
 			cli.New,
 			config.New,
-			app.New,
+			logger.New,
+			postgres.New,
+			server.NewGRPC,
+			server.NewREST,
 		),
 
-		fx.Invoke(app.Run),
+		fx.Invoke(
+			func(grpc *server.GRPC, rest *server.REST) {
+				go grpc.Init()
+				go func() {
+					err := grpc.Run()
+					if err != nil {
+						log.Fatal(err)
+					}
+				}()
+
+				go func() {
+					err := rest.Init()
+					if err != nil {
+						log.Fatal(err)
+					}
+				}()
+				go func() {
+					err := rest.Run()
+					if err != nil {
+						log.Fatal(err)
+					}
+				}()
+			},
+		),
 	).Run()
 }
