4fdfbff6cfa41ead0e0bb59a2e6ce57137d580a8
Sergey Erofeev <erofsey@yandex.kz>
2024-02-24 07:20:23 +0600

    Refactoring jwt

--- internal/core/jwt/jwt.go
+++ internal/core/jwt/jwt.go
@@ -1,22 +1,30 @@
 package jwt
 
 import (
+	"context"
 	"log/slog"
 	"strconv"
+	"strings"
 	"time"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
+	"github.com/eliofery/golang-grpc/pkg/eslog"
 	"github.com/golang-jwt/jwt/v5"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
 
-	"github.com/eliofery/golang-fullstack/pkg/eslog"
+const (
+	bearerPrefix  = "Bearer "
+	authHeaderKey = "Authorization"
 )
 
 var (
-	errSignToken    = status.Error(codes.Internal, "failed to sign token")
+	errCreateToken  = status.Error(codes.Internal, "failed create token")
+	errGetToken     = status.Error(codes.Internal, "failed get token")
 	errInvalidToken = status.Error(codes.InvalidArgument, "invalid token")
-	errUserData     = status.Error(codes.Internal, "failed to get user data")
+	errUserData     = status.Error(codes.Internal, "failed get user data")
 )
 
 // TokenManager ...
@@ -34,19 +42,18 @@ func New(config *Config, logger *eslog.Logger) *TokenManager {
 }
 
 // Generate ...
-func (t *TokenManager) Generate(userID *int64) (string, error) {
+func (t *TokenManager) Generate(userID int64) (string, error) {
 	op := "core.jwt.Generate"
 
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": strconv.FormatInt(*userID, 10),
+		"sub": strconv.FormatInt(userID, 10),
 		"exp": time.Now().Add(time.Second * t.config.Expires).Unix(),
 	})
 
 	token, err := claims.SignedString([]byte(t.config.Secret))
 	if err != nil {
 		t.logger.Debug(op, slog.String("err", err.Error()))
-
-		return "", errSignToken
+		return "", errCreateToken
 	}
 
 	return token, nil
@@ -59,7 +66,6 @@ func (t *TokenManager) Verify(token string) (claims jwt.MapClaims, err error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			t.logger.Debug(op, slog.String("err", "token signing method must be: *jwt.SigningMethodHMAC"))
-
 			return nil, errInvalidToken
 		}
 
@@ -68,14 +74,12 @@ func (t *TokenManager) Verify(token string) (claims jwt.MapClaims, err error) {
 
 	if err != nil || !parsedToken.Valid {
 		t.logger.Debug(op, slog.String("err", err.Error()))
-
 		return nil, errInvalidToken
 	}
 
 	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 	if !ok {
 		t.logger.Debug(op, slog.String("err", "token claims must be: jwt.MapClaims"))
-
 		return nil, errInvalidToken
 	}
 
@@ -83,20 +87,56 @@ func (t *TokenManager) Verify(token string) (claims jwt.MapClaims, err error) {
 }
 
 // GetSubject ...
-func (t *TokenManager) GetSubject(claims jwt.MapClaims) (*int64, error) {
+func (t *TokenManager) GetSubject(claims jwt.MapClaims) (int64, error) {
 	op := "core.jwt.GetSubject"
 
 	userID, err := claims.GetSubject()
 	if err != nil {
 		t.logger.Debug(op, slog.String("err", err.Error()))
-		return nil, errUserData
+		return 0, errUserData
 	}
 
 	id, err := strconv.ParseInt(userID, 10, 64)
 	if err != nil {
 		t.logger.Debug(op, slog.String("err", err.Error()))
-		return nil, errUserData
+		return 0, errUserData
 	}
 
-	return &id, nil
+	return id, nil
+}
+
+// SendAuthHeader ...
+func (t *TokenManager) SendAuthHeader(ctx context.Context, token string) error {
+	op := "core.jwt.SendAuthHeader"
+
+	if err := grpc.SendHeader(ctx, metadata.Pairs(authHeaderKey, bearerPrefix+token)); err != nil {
+		t.logger.Debug(op, slog.String("err", err.Error()))
+		return errGetToken
+	}
+
+	return nil
+}
+
+// GetAuthHeader ...
+func (t *TokenManager) GetAuthHeader(ctx context.Context) (string, error) {
+	op := "core.jwt.GetAuthHeader"
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		t.logger.Debug(op, slog.String("err", "metadata not provided"))
+		return "", errGetToken
+	}
+
+	authHeader := md.Get(authHeaderKey)
+	if authHeader == nil {
+		t.logger.Debug(op, slog.String("err", "authorization header not provided"))
+		return "", errGetToken
+	}
+
+	if !strings.HasPrefix(authHeader[0], bearerPrefix) {
+		t.logger.Debug(op, slog.String("err", "invalid authorization header format"))
+		return "", errGetToken
+	}
+
+	return strings.TrimPrefix(authHeader[0], bearerPrefix), nil
 }
