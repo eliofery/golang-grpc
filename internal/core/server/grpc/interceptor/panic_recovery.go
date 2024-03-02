9fd018c1234151c806d8ef3a087fa80febc6a01e
Sergey Erofeev <erofsey@yandex.kz>
2024-03-02 16:31:28 +0500

    Added interceptor panic recovery

---
+++ internal/core/server/grpc/interceptor/panic_recovery.go
@@ -0,0 +1,30 @@
+package interceptor
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"runtime/debug"
+
+	"github.com/eliofery/golang-grpc/pkg/eslog"
+	"google.golang.org/grpc"
+)
+
+// panicRecovery ...
+func panicRecovery(logger *eslog.Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		defer func() {
+			if r := recover(); r != nil {
+				logger.Error("Recovered from panic", slog.Any("err", r))
+
+				buf := new(bytes.Buffer)
+				stack := debug.Stack()
+				buf.Write(stack)
+
+				logger.Error("Stack trace", slog.String("err", buf.String()))
+			}
+		}()
+
+		return handler(ctx, req)
+	}
+}
