c512c8aa13de2f7ff5450403ed2f7dbfb3e645a6
Sergey Erofeev <erofsey@yandex.kz>
2024-03-03 07:17:17 +0500

    Refactoring interceptor (user data from token)

--- internal/core/server/grpc/interceptor/interceptor.go
+++ internal/core/server/grpc/interceptor/interceptor.go
@@ -1,15 +1,21 @@
 package interceptor
 
 import (
-	"github.com/eliofery/golang-fullstack/internal/core/jwt"
-	"github.com/eliofery/golang-fullstack/pkg/eslog"
+	tokenv1 "github.com/eliofery/golang-grpc/internal/app/v1app/denied_token/repository"
+	"github.com/eliofery/golang-grpc/internal/core/jwt"
+	"github.com/eliofery/golang-grpc/pkg/eslog"
 	"google.golang.org/grpc"
 )
 
 // New ...
-func New(logger *eslog.Logger, tokenManager *jwt.TokenManager) []grpc.UnaryServerInterceptor {
+func New(
+	logger *eslog.Logger,
+	tokenManager *jwt.TokenManager,
+	tokenRepository tokenv1.Repository,
+) []grpc.UnaryServerInterceptor {
 	return []grpc.UnaryServerInterceptor{
+		panicRecovery(logger),
 		validate(logger),
-		userIDFromToken(logger, tokenManager),
+		userDataFromAuthHeader(logger, tokenManager, tokenRepository),
 	}
 }
---
+++ internal/core/server/grpc/interceptor/user_data_from_auth_header.go
@@ -0,0 +1,112 @@
+package interceptor
+
+import (
+	"context"
+	"log/slog"
+
+	deniedTokenV1Repository "github.com/eliofery/golang-grpc/internal/app/v1app/denied_token/repository"
+	"github.com/eliofery/golang-grpc/internal/core/jwt"
+	"github.com/eliofery/golang-grpc/pkg/eslog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type key string
+
+const userKey key = "user"
+
+var (
+	// ErrAlreadyAuthenticated ...
+	ErrAlreadyAuthenticated = status.Error(codes.PermissionDenied, "you are already authenticated")
+
+	// ErrNotAuthenticated ...
+	ErrNotAuthenticated = status.Error(codes.PermissionDenied, "you are not authenticated")
+)
+
+// UserData ...
+type UserData struct {
+	ID     int64
+	Token  string
+	RoleID int64
+}
+
+// userDataFromAuthHeader ...
+func userDataFromAuthHeader(
+	logger *eslog.Logger,
+	tokenManager *jwt.TokenManager,
+
+	deniedTokenV1Repository deniedTokenV1Repository.Repository,
+) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		op := "core.server.grpc.interceptor.userDataFromAuthHeader"
+
+		token, err := tokenManager.GetAuthHeader(ctx)
+		if err != nil {
+			return handler(ctx, req)
+		}
+
+		claims, err := tokenManager.Verify(token)
+		if err != nil {
+			logger.Debug(op, slog.String("err", err.Error()))
+			return handler(ctx, req)
+		}
+
+		if deniedToken, _ := deniedTokenV1Repository.GetByToken(ctx, token); deniedToken != nil {
+			logger.Debug(op, slog.String("err", "denied token"))
+			return handler(ctx, req)
+		}
+
+		userID, err := tokenManager.GetSubject(claims)
+		if err != nil {
+			logger.Debug(op, slog.String("err", err.Error()))
+			return handler(ctx, req)
+		}
+
+		user := UserData{
+			ID:    userID,
+			Token: token,
+		}
+
+		return handler(withUser(ctx, &user), req)
+	}
+}
+
+// withUser ...
+func withUser(ctx context.Context, user *UserData) context.Context {
+	return context.WithValue(ctx, userKey, user)
+}
+
+// User ...
+func User(ctx context.Context) *UserData {
+	user, ok := ctx.Value(userKey).(*UserData)
+	if ok {
+		return user
+	}
+
+	return nil
+}
+
+// UserID ...
+func UserID(ctx context.Context, reqID ...int64) int64 {
+	if len(reqID) > 0 && reqID[0] != 0 {
+		return reqID[0]
+	}
+
+	return User(ctx).ID
+}
+
+// UserToken ...
+func UserToken(ctx context.Context) string {
+	return User(ctx).Token
+}
+
+// UserRoleID ...
+func UserRoleID(ctx context.Context) int64 {
+	return User(ctx).RoleID
+}
+
+// IsAuthenticated ...
+func IsAuthenticated(ctx context.Context) bool {
+	return User(ctx) != nil
+}
