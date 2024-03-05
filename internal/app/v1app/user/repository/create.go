41eb8b2e256e9cf09c941eb01438dfd46b6eaf07
Sergey Erofeev <erofsey@yandex.kz>
2024-03-05 07:06:38 +0500

    Refactoring user repository

--- internal/app/v1app/user/repository/create.go
+++ internal/app/v1app/user/repository/create.go
@@ -3,45 +3,52 @@ package repository
 import (
 	"context"
 	"errors"
+	"log/slog"
 
-	"github.com/eliofery/golang-fullstack/internal/app/v1app/user/model"
-	"github.com/eliofery/golang-fullstack/internal/core/database/postgres"
+	"github.com/eliofery/golang-grpc/internal/app/v1app/user/dto"
+	"github.com/eliofery/golang-grpc/internal/app/v1app/user/model"
+	"github.com/eliofery/golang-grpc/internal/core/database/postgres"
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 // Create ...
-func (r *repository) Create(ctx context.Context, userInfo *model.UserInfo) (*int64, error) {
-	qb := r.pgQb().
+func (r *repository) Create(ctx context.Context, user *dto.User) (int64, error) {
+	op := "v1.user.repository.User"
+
+	qb := r.pgQb.
 		Insert(model.TableName).
-		PlaceholderFormat(r.format).
-		Columns(model.ColumnFirstName, model.ColumnLastName, model.ColumnEmail, model.ColumnPassword).
-		Values(userInfo.FirstName, userInfo.LastName, userInfo.Email, userInfo.Password).
+		Columns(model.ColumnFirstName, model.ColumnLastName, model.ColumnEmail, model.ColumnPassword, model.ColumnRoleID).
+		Values(user.FirstName, user.LastName, user.Email, user.Password, user.RoleID).
 		Suffix("RETURNING id")
 
 	query, args, err := qb.ToSql()
 	if err != nil {
-		return nil, err
+		r.logger.Debug(op, slog.String("err", err.Error()))
+		return 0, errCreate
 	}
 
 	q := postgres.Query{
-		Name:     "v1.user.repository.Create",
+		Name:     op,
 		QueryRaw: query,
 	}
 
 	var id int64
 	if err = r.db.QueryRowContext(ctx, q, args...).Scan(&id); err != nil {
+		r.logger.Debug(op, slog.String("err", err.Error()))
+
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) {
-			if pgErr.Code == pgerrcode.UniqueViolation {
-				return nil, status.Error(codes.AlreadyExists, "email already exists")
+			switch pgErr.Code {
+			case pgerrcode.UniqueViolation:
+				return 0, errExists
+			case pgerrcode.ForeignKeyViolation:
+				return 0, errRoleID
 			}
 		}
 
-		return nil, errors.New("failed to create user")
+		return 0, errCreate
 	}
 
-	return &id, nil
+	return id, nil
 }
--- internal/app/v1app/user/repository/repository.go
+++ internal/app/v1app/user/repository/repository.go
@@ -4,29 +4,49 @@ import (
 	"context"
 
 	"github.com/Masterminds/squirrel"
-	"github.com/eliofery/golang-fullstack/internal/app/v1app/user/model"
-	"github.com/eliofery/golang-fullstack/internal/core/database/postgres"
+	"github.com/eliofery/golang-grpc/internal/app/v1app/user/dto"
+	"github.com/eliofery/golang-grpc/internal/app/v1app/user/model"
+	"github.com/eliofery/golang-grpc/internal/core/database/postgres"
+	"github.com/eliofery/golang-grpc/internal/core/pagination"
+	"github.com/eliofery/golang-grpc/pkg/eslog"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	errCreate               = status.Error(codes.Internal, "failed to create user")
+	errUpdate               = status.Error(codes.Internal, "failed to update user")
+	errDelete               = status.Error(codes.Internal, "failed to delete user")
+	errExists               = status.Error(codes.AlreadyExists, "user already exists")
+	errRoleID               = status.Error(codes.InvalidArgument, "role not exists")
+	errGetByEmail           = status.Error(codes.Internal, "failed to get user by email")
+	errGetByID              = status.Error(codes.Internal, "failed to get user by id")
+	errGetAll               = status.Error(codes.Internal, "failed to get users")
+	errWrongLoginOrPassword = status.Error(codes.NotFound, "wrong login or password")
+	errNotFound             = status.Error(codes.NotFound, "user not found")
 )
 
 // Repository ...
 type Repository interface {
-	Create(context.Context, *model.UserInfo) (*int64, error)
+	GetByID(context.Context, int64) (*model.User, error)
+	GetByEmail(context.Context, string) (*model.User, error)
+	GetAll(context.Context, *pagination.Pagination) ([]model.User, error)
+	Create(context.Context, *dto.User) (int64, error)
+	Update(context.Context, *dto.Update) (*model.User, error)
+	Delete(context.Context, int64) error
 }
 
 type repository struct {
 	db     postgres.DB
-	format squirrel.PlaceholderFormat
+	pgQb   squirrel.StatementBuilderType
+	logger *eslog.Logger
 }
 
 // New ...
-func New(pg postgres.Client) Repository {
+func New(pg postgres.Client, logger *eslog.Logger) Repository {
 	return &repository{
 		db:     pg.DB(),
-		format: squirrel.Dollar,
+		pgQb:   pg.QB(),
+		logger: logger,
 	}
-}
-
-// pgQb ...
-func (r *repository) pgQb() squirrel.StatementBuilderType {
-	return squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
 }
