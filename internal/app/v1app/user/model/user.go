9ad1a6216a9823b0fbab554a5e8adf17a2a15920
Sergey Erofeev <erofsey@yandex.kz>
2024-03-05 07:05:51 +0500

    Refactoring user model

--- internal/app/v1app/user/model/user.go
+++ internal/app/v1app/user/model/user.go
@@ -14,14 +14,19 @@ const (
 	ColumnLastName  = "last_name"
 	ColumnEmail     = "email"
 	ColumnPassword  = "password"
+	ColumnRoleID    = "role_id"
 	ColumnCreatedAt = "created_at"
 	ColumnUpdatedAt = "updated_at"
 )
 
 // User ...
 type User struct {
-	ID        int64 `db:"id"`
-	UserInfo  `db:""`
-	CreatedAt time.Time    `db:"created_at"`
-	UpdatedAt sql.NullTime `db:"updated_at"`
+	ID        int64          `db:"id"`
+	FirstName sql.NullString `db:"first_name"`
+	LastName  sql.NullString `db:"last_name"`
+	Email     string         `db:"email"`
+	Password  string         `db:"password"`
+	RoleID    int64          `db:"role_id"`
+	CreatedAt time.Time      `db:"created_at"`
+	UpdatedAt sql.NullTime   `db:"updated_at"`
 }
