c8a6e79c669315ab0cc08cac3ea00953e7e845f8
Sergey Erofeev <erofsey@yandex.kz>
2024-03-15 19:02:44 +0500

    Added role permission module

--- internal/app/v1app/v1app.go
+++ internal/app/v1app/v1app.go
@@ -1,9 +1,10 @@
 package v1app
 
 import (
-	deniedtoken "github.com/eliofery/golang-grpc/internal/app/v1app/denied_token"
+	deniedToken "github.com/eliofery/golang-grpc/internal/app/v1app/denied_token"
 	"github.com/eliofery/golang-grpc/internal/app/v1app/permission"
 	"github.com/eliofery/golang-grpc/internal/app/v1app/role"
+	rolePermission "github.com/eliofery/golang-grpc/internal/app/v1app/role_permission"
 	"github.com/eliofery/golang-grpc/internal/app/v1app/setting"
 	"github.com/eliofery/golang-grpc/internal/app/v1app/user"
 	"go.uber.org/fx"
@@ -13,9 +14,10 @@ import (
 func NewModule() fx.Option {
 	return fx.Module("app_v1",
 		setting.NewModule(),
-		deniedtoken.NewModule(),
+		deniedToken.NewModule(),
 		role.NewModule(),
 		user.NewModule(),
 		permission.NewModule(),
+		rolePermission.NewModule(),
 	)
 }
