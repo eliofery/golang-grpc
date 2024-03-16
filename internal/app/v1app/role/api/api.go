534060c5816fb52e88f6a3d7837a98812d7ce1c3
Sergey Erofeev <erofsey@yandex.kz>
2024-03-16 07:24:50 +0500

    Refactoring role api

--- internal/app/v1app/role/api/api.go
+++ internal/app/v1app/role/api/api.go
@@ -1,19 +1,19 @@
 package api
 
 import (
-	rolev1 "github.com/eliofery/golang-grpc/internal/app/v1app/role/service"
+	role "github.com/eliofery/golang-grpc/internal/app/v1app/role/service"
 	desc "github.com/eliofery/golang-grpc/pkg/api/role/v1"
 )
 
 // api ...
 type api struct {
 	desc.UnimplementedRoleV1ServiceServer
-	roleService rolev1.Service
+	roleService role.Service
 }
 
 // New ...
 func New(
-	roleService rolev1.Service,
+	roleService role.Service,
 ) desc.RoleV1ServiceServer {
 	return &api{
 		roleService: roleService,
