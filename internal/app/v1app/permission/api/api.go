c7ce33ae2f0fd16bd322395e1b0cca6b32437e04
Sergey Erofeev <erofsey@yandex.kz>
2024-03-18 22:10:20 +0500

    Refactoring permission api

--- internal/app/v1app/permission/api/api.go
+++ internal/app/v1app/permission/api/api.go
@@ -1,19 +1,19 @@
 package api
 
 import (
-	permissionv1 "github.com/eliofery/golang-grpc/internal/app/v1app/permission/service"
+	permission "github.com/eliofery/golang-grpc/internal/app/v1app/permission/service"
 	desc "github.com/eliofery/golang-grpc/pkg/api/permission/v1"
 )
 
 // api ...
 type api struct {
 	desc.UnimplementedPermissionV1ServiceServer
-	permissionService permissionv1.Service
+	permissionService permission.Service
 }
 
 // New ...
 func New(
-	permissionService permissionv1.Service,
+	permissionService permission.Service,
 ) desc.PermissionV1ServiceServer {
 	return &api{
 		permissionService: permissionService,
--- internal/app/v1app/permission/api/delete.go
+++ internal/app/v1app/permission/api/delete.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	"github.com/eliofery/golang-grpc/internal/core/server/grpc/interceptor"
-	v1 "github.com/eliofery/golang-grpc/pkg/api/permission/v1"
+	desc "github.com/eliofery/golang-grpc/pkg/api/permission/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func (a *api) Delete(ctx context.Context, req *v1.DeleteRequest) (*emptypb.Empty, error) {
+func (a *api) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
 	if !interceptor.IsAuthenticated(ctx) {
 		return nil, interceptor.ErrNotAuthenticated
 	}
